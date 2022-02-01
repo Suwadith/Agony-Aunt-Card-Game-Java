ffcbd414af8541e0947e014c8c1dd2e094de7964
Suwadith <suwadith.srithar@gmail.com>
2022-02-01 23:12:41 +0000

    Map created with Counter as key and count(integer) as value. ToDiscuss: Have to finalize the design for the PenaltySquares class and the Penalty abstract class.

--- src/model/PenaltySquares.java
+++ src/model/PenaltySquares.java
@@ -1,8 +1,11 @@
 package model;
 
+import java.util.Map;
+
 public class PenaltySquares {
 
     private String penaltyName;
+	private Map<Counter, Integer> counters;
 
 	public PenaltySquares(String penaltyName) {
 		this.penaltyName = penaltyName;
@@ -11,5 +14,12 @@ public class PenaltySquares {
 	public String getPenaltyName() {
 		return penaltyName;
 	}
-    
+
+	public Map<Counter, Integer> getCounters() {
+		return counters;
+	}
+
+	public void setCounters(Map<Counter, Integer> counters) {
+		this.counters = counters;
+	}
 }
